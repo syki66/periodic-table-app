b8df63fb529571bb32aae2803805f311577d9641
syki66 <66syki@gmail.com>
2019-06-20 00:17:05 +0900

    update NewJugiyul

---
+++ app/src/main/java/com/example/newjugiyul/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.newjugiyul;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // MainActivity.class 자리에 다음에 넘어갈 액티비티를 넣어주기
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("state", "launch");
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        //초반 플래시 화면에서 넘어갈때 뒤로가기 버튼 못누르게 함
+    }
+
+}
